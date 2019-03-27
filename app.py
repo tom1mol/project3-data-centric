ac433bf2532aa29051bf01b5ec85a77f7f0b2ed4
tom1mol <tomasomaolmhuaidh@gmail.com>
2019-03-27 16:06:22 +0000

    added tasks.html

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = 'project3-data-centric'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+#app.config["MONGO_URI"] = 'mongodb+srv://shoot:<pass>@myfirstcluster-tjr5r.mongodb.net/project3-data-centric?retryWrites=true'
+
+    
+mongo = PyMongo(app)
+
+
 @app.route('/')             # / refers to default route
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find()) 
 
                             #set up ip address and port num
 if __name__ == '__main__':
